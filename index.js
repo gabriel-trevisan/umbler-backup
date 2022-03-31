c6e24f3a3ec5771566b1d442ccad0a726a92fee6
Gabriel Santos <gabriel.santos@hidroall.com.br>
2022-03-31 14:34:42 -0300

    Login in platform phpmyadmin

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
+require('dotenv/config');
 const puppeteer = require('puppeteer');
 
-async function run(){
+async function run() {
+
+    const url = 'https://phpmyadmin.umbler.com/';
+    const server = process.env.SERVER;
+    const user = process.env.USER;
+    const password = process.env.PASSWORD;
+
+    if (server === "") {
+        console.log("Server must not be empty");
+        return;
+    }
+
+    if (user === "") {
+        console.log("User must not be empty");
+        return;
+    }
+
+    if (password === "") {
+        console.log("Password must not be empty");
+        return;
+    }
+
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
 
-    await page.goto('https://app.umbler.com/');
+    await page.goto(url);
+
+    await page.type('#input_servername', process.env.SERVER);
+    await page.type('#input_username', process.env.USER);
+    await page.type('#input_password', process.env.PASSWORD);
+    await page.click('#input_go');
+    await page.waitForNavigation();
 
     await browser.close();
 }
